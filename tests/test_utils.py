302cefc7c1ce83eeecab1c202caff0690f283472
Akash A Desai <62583018+akashAD98@users.noreply.github.com>
2022-07-28 18:33:17 +0530

    Add files via upload

---
+++ tests/test_utils.py
@@ -0,0 +1,5 @@
+class TestConstants:
+    YOLOV7_MODEL_PATH = "yolov7/weights/yolov7.pt"
+    YOLOv7_W6_MODEL_PATH = "yolov7/weights/yolov7-w6.pt"
+    ZIDANE_IMAGE_PATH = "tests/data/zidane.jpg"
+    BUS_IMAGE_PATH = "tests/data/bus.jpg"
