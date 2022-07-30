a262c155a8d3f613657cf3ce6da1ae1644e0e825
Akash A Desai <62583018+akashAD98@users.noreply.github.com>
2022-07-30 10:37:29 +0530

    Update export.py

--- yolov7/models/export.py
+++ yolov7/models/export.py
@@ -8,10 +8,10 @@ import torch
 import torch.nn as nn
 
 import models
-from models.experimental import attempt_load
-from utils.activations import Hardswish, SiLU
-from utils.general import set_logging, check_img_size
-from utils.torch_utils import select_device
+from yolov7.models.experimental import attempt_load
+from yolov7.utils.activations import Hardswish, SiLU
+from yolov7.utils.general import set_logging, check_img_size
+from yolov7.utils.torch_utils import select_device
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
