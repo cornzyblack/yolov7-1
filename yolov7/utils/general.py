9913b06ed737f282901a27d795deaa90b18e315d
Akash A Desai <62583018+akashAD98@users.noreply.github.com>
2022-08-23 19:05:17 +0530

    Update general.py

--- yolov7/utils/general.py
+++ yolov7/utils/general.py
@@ -19,7 +19,7 @@ import torchvision
 import yaml
 
 from yolov7.utils.google_utils import gsutil_getsize
-from yolov7utils.metrics import fitness
+from yolov7.utils.metrics import fitness
 from yolov7.utils.torch_utils import init_torch_seeds
 
 # Settings
