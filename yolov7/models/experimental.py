53b3e927f6584e6cdd61c7cc2c1a43775519fdc9
Adah Eni <eniwokepdm@gmail.com>
2022-08-26 15:32:56 +0100

    refactor: add attempt_download, and  map_location=cpu

--- yolov7/models/experimental.py
+++ yolov7/models/experimental.py
@@ -99,8 +99,7 @@ def attempt_load(weights, map_location=None, fuse=True):
     model = Ensemble()
     for w in weights if isinstance(weights, list) else [weights]:
         with yolov7_in_syspath():
-            attempt_download(w)
-            ckpt = torch.load(w, map_location=map_location)  # load
+            ckpt = torch.load(attempt_download(weights), map_location="cpu")  # load
         device = "cuda" if torch.cuda.is_available() else "cpu"
         ckpt = (ckpt.get("ema") or ckpt["model"]).to(device).float()  # FP32 model
         # model.append(ckpt['ema' if ckpt.get('ema') else 'model'].float().fuse().eval())  # FP32 model
